77131d28fdb4a1c4f5c9d5084387c04ed2c362ac
Qing-Cheng Li <qc.linux@gmail.com>
2014-04-04 16:25:29 +0800

    build the rnage of each dump

--- wikidump/buildTargetRange.py
+++ wikidump/buildTargetRange.py
@@ -4,24 +4,25 @@
 import sys
 import os
 
-f = open("../dbpedia/dbpedia.instance.sorted.id.txt","r")
+#f = open("../dbpedia/dbpedia.instance.sorted.id.txt","r")
+f = open("../dbpedia/dbpedia.instance.sorted.revid.txt","r")
 i = 1
 r = open("./range/enwiki.01.range","r")
-o = open("./range/target.01.id","w")
+o = open("./range/target.01.revid","w")
 s = int(r.readline())
 e = int(r.readline())
 
 for line in f:
-    t = int(line)
+    t = int(line.split("\t")[0])
     if t <= e:
-        o.write(str(t)+"\n")
+        o.write(line)
     else:
         i += 1
         print "enwiki-%02d" % (i)
         o.close()
         r.close()
-        o = open("./range/target.%02d.id" % (i),"w")
+        o = open("./range/target.%02d.revid" % (i),"w")
         r = open("./range/enwiki.%02d.range" % (i),"r")
         s = int(r.readline())
         e = int(r.readline())
-        o.write(str(t)+"\n")
+        o.write(line+"\n")
