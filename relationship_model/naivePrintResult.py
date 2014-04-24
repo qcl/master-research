0e061d004f0648893a671925fb304e8f424f581d
Qing-Cheng Li <qc.linux@gmail.com>
2014-04-24 22:55:58 +0800

    fix the logic error

--- relationship_model/naivePrintResult.py
+++ relationship_model/naivePrintResult.py
@@ -22,50 +22,60 @@ def main(inputResultFile,outputformat):
         result = json.load(open(os.path.join(inputResultFile,filename),"r"))
         for model in result:
             m = result[model]
-            if float(m["tp"]+m["fp"]) == .0:
-                pre = .0
-            else:
-                pre = float(m["tp"])/float(m["tp"]+m["fp"])
 
-            if float(m["tp"]+m["fn"]) == .0:
-                rec = .0
-            else:
-                rec = float(m["tp"])/float(m["tp"]+m["fn"])
+            if not model in r:
+                r[model] = {"fp":0,"tn":0,"tp":0,"fn":0}
+            j = r[model]
+            j["tp"] += m["tp"]
+            j["tn"] += m["tn"]
+            j["fp"] += m["fp"]
+            j["fn"] += m["fn"]
+
+    for model in r:
+        m = r[model]
+
+        if float(m["tp"]+m["fp"]) == .0:
+            pre = .0
+        else:
+            pre = float(m["tp"])/float(m["tp"]+m["fp"])
+
+        if float(m["tp"]+m["fn"]) == .0:
+            rec = .0
+        else:
+            rec = float(m["tp"])/float(m["tp"]+m["fn"])
         
-            if float(m["tp"]+m["tn"]+m["fp"]+m["fn"]) == .0:
-                acc = .0
-            else:
-                acc = float(m["tp"]+m["tn"])/float(m["tp"]+m["tn"]+m["fp"]+m["fn"])
-            if pre+rec == .0:
-                fsc = .0
-            else:
-                fsc = (2*pre*rec)/(pre+rec)
-            r[model] = {"p":pre,"r":rec,"a":acc,"n":model,"f":fsc}
+        if float(m["tp"]+m["tn"]+m["fp"]+m["fn"]) == .0:
+            acc = .0
+        else:
+            acc = float(m["tp"]+m["tn"])/float(m["tp"]+m["tn"]+m["fp"]+m["fn"])
+        
+        if pre+rec == .0:
+            fsc = .0
+        else:
+            fsc = (2*pre*rec)/(pre+rec)
+        
+        m["p"] = pre
+        m["r"] = rec
+        m["a"] = acc
+        m["n"] = model
+        m["f"] = fsc
     
+    # output
     for model in r:
         res = r[model]
         string = ""
         if outputformat[0] == "n":
             string = "%s" % (res["n"])
-            if len(res["n"]) < 4:
-                string += "\t"
-            if len(res["n"]) < 8:
-                string += "\t"
-            if len(res["n"]) < 12:
-                string += "\t"
-            if len(res["n"]) < 16:
-                string += "\t"
-            if len(res["n"]) < 20:
-                string += "\t"
-            if len(res["n"]) < 24:
-                string += "\t"
-
+        elif outputformat[0] == "t":
+            string = "%d\t%d\t%d\t%d" % (res["tp"],res["fn"],res["fp"],res["fn"] )
         else:
             string = "%.08f" % (res[outputformat[0]])
 
         for x in xrange(1,len(outputformat)):
             if outputformat[x] == "n":
                 string += "\t%s" % (res[outputformat[x]])
+            elif outputformat[x] == "t":
+                string += "%d\t%d\t%d\t%d" % (res["tp"],res["fn"],res["fp"],res["fn"] )
             else:
                 string += "\t%.08f" % (res[outputformat[x]])
 
@@ -78,4 +88,4 @@ if __name__ == "__main__":
         main(inputResultFile,outputformat)
     else:
         print "$ python ./naivePrintResult.py input.json [prn|rpn|npr|nrp]"
-        print "p = precision, r = recall, a = accuracy, f = f1, n = relationship name"
+        print "p = precision, r = recall, a = accuracy, f = f1, n = relationship name, t = tp,fn,fp,tn"
