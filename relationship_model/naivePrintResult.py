383d09f77333be3dbbe1d8f37be547dc1c8b59bc
Qing-Cheng Li <qc.linux@gmail.com>
2014-04-24 23:05:23 +0800

    fix the fucking typo

--- relationship_model/naivePrintResult.py
+++ relationship_model/naivePrintResult.py
@@ -75,7 +75,7 @@ def main(inputResultFile,outputformat):
             if outputformat[x] == "n":
                 string += "\t%s" % (res[outputformat[x]])
             elif outputformat[x] == "t":
-                string += "\t%d\t%d\t%d\t%d" % (res["tp"],res["fn"],res["fp"],res["fn"] )
+                string += "\t%d\t%d\t%d\t%d" % (res["tp"],res["fn"],res["fp"],res["tn"] )
             else:
                 string += "\t%.08f" % (res[outputformat[x]])
 
