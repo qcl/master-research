659b12570fed3d91790466c1853fe2b8f53492c5
Qing-Cheng Li <qc.linux@gmail.com>
2014-05-02 00:31:47 +0800

    add search algo

--- prefix_tree_model/treeModelRun.py
+++ prefix_tree_model/treeModelRun.py
@@ -6,6 +6,7 @@
 
 import os
 import re
+import gc
 import sys
 import nltk
 import time
@@ -38,7 +39,27 @@ class SelfDoingTokenizer(nltk.tokenize.api.TokenizerI):
             yield span
 
 
-def filterFiles(jobid,filename,treeModel,debug):
+def filterFiles(jobid,filename,dataInputPath,resultOutPath,treeModel,debug):
+
+    # get the pos tag name
+    def getPosTagName(pos):
+        tagName = "_na_"
+        if pos[-2:] == "DT":    # [[det]]
+            tagName = "[[det]]"
+        elif pos[:2] == "JJ":   # [[adj]]
+            tagName = "[[adj]]"
+        elif pos[:3] == "PRP":  # [[pro]]
+            tagName = "[[pro]]"
+        elif pos == "CD":       # [[num]]
+            tagName = "[[num]]"
+        elif pos == "CC":       # [[con]]
+            tagName = "[[pos]]"
+        elif pos == "MD":       # [[mod]]
+            tagName = "[[mod]]"
+        elif pos == "IN":       # [[prp]]
+            tagName = "[[prp]]"
+        return tagName
+
     content = json.load(open(os.path.join(dataInputPath,filename),"r"))
     print "Worker %d : Read %s into filter" % (jobid,filename)
     postagger = PerceptronTagger()
@@ -47,7 +68,10 @@ def filterFiles(jobid,filename,treeModel,debug):
     count = 0
     dealL = 0
     exception = False
+    results = {}
     for subFilename in content:
+
+        result = {}
 
         # start to read a article.
         pt = treeModel
@@ -65,72 +89,95 @@ def filterFiles(jobid,filename,treeModel,debug):
                 break
     
             # Here start the core algorithm
+            travarsingTrees = {}
 
-            inPattern = False
-
+            treeCount = 0
             for word,pos in posSet:
-                print word,pos,"  ",
+                #print word,pos,"  ",
                 
-                inTree = False
-                tagName = ""
-                # test word
-                if word in pt:
-                    inTree = True
-                # test pos
-                else:
-                    if pos[-2:] == "DT":
-                        # [[det]]
-                        tagName = "[[det]]"
-                    elif pos[:2] == "JJ":
-                        # [[adj]]
-                        tagName = "[[adj]]"
-                    elif pos[:3] == "PRP":
-                        # [[pro]]
-                        tagName = "[[pro]]"
-                    elif pos == "CD":
-                        # [[num]]
-                        tagName = "[[num]]"
-                    elif pos == "CC":
-                        # [[con]]
-                        tagName = "[[pos]]"
-                    elif pos == "MD":
-                        # [[mod]]
-                        tagName = "[[mod]]"
-                    elif pos == "IN":
-                        # [[prp]]
-                        tagName = "[[prp]]"
-                    if len(tagName) > 0:
-                        if tagName in pt:
-                            inTree = True
+                tagName = getPosTagName(pos)
+                
+                treeByWord = -1
+                treeByPOS  = -1
 
+                # If this node is a root 
+                if word in treeModel:
+                    treeByWord = treeCount
+                    travarsingTrees[treeCount] = treeModel
+                    treeCount+=1
+                    
+                if tagName in treeModel:
+                    treeByPOS = treeCount
+                    travarsingTrees[treeCount] = treeModel
+                    treeCount+=1
 
-                if inTree:
-                    if inPattern:
-                        pass
+                needRemove = []
+                needAppend = []
+                for treeId in travarsingTrees:
+                    tree = travarsingTrees[treeId] 
+               
+                    # 先收割
+                    if "_rls_" in tree:
+                        for relation in tree["_rls_"]:
+                            if not relation in result:
+                                result[relation] = 0
+                            result[relation] += 1
+
+                    if treeId == treeByWord:
+                        travarsingTrees[treeId] = tree[word]
+                    elif treeId == treeByPOS:
+                        travarsingTrees[treeId] = tree[tagName]
                     else:
-                        pass
-                else:
-                    if inPattern:
-                        pass
-                    else:
-                        pass
-                    # not in target set.
+                        inWord = False
+                        inTag  = False
+                        if word in tree:
+                            inWord = True
+                            travarsingTrees[treeId] = tree[word]
+
+                        if tagName in tree:
+                            inTag = True
+                            if inWord:
+                                needAppend.append((treeCount,tree[tagName]))
+                                treeCount += 1
+                            else:
+                                travarsingTrees[treeId] = tree[tagName]
+                        
+                        if not inWord and not inTag:
+                            needRemove.append(treeId)
+   
+                #print len(travarsingTrees),len(needRemove),len(needAppend)
+
+                for treeId in needRemove:
+                    travarsingTrees.pop(treeId)
+                for treeId,tree in needAppend:
+                    travarsingTrees[treeId] = tree
 
             # end of core algorithm
 
             dealL += 1
+            #print line
             
             if dealL % 10000 == 0:
                 print "Worker %d deal with %d lines." % (jobid,dealL)
+        
+        # end of article
+        results[subFilename] = result
+        # print subFilename,result
+
         count += 1
         if count % 100 == 0:
             print "Worker %d deal with %d files" % (jobid,count)
+            gc.collect()
             if debug:
                 break
 
         if exception:
             print "Worker %d exception @ %d / %d " % (jobid,dealL,count)
             break
+
+    # write the result out.
+    json.dump(results,open(os.path.join(resultOutPath,filename),"w"))
+    print "Worker %d : Write results out to %s." % (jobid,filename)
 
 
 def main(treeModelPath,dataInputPath,resultOutPath,debug):
@@ -154,10 +201,13 @@ def main(treeModelPath,dataInputPath,resultOutPath,debug):
     jobN = 0 
     for filename in os.listdir(dataInputPath):
         if ".json" in filename:
-            pool.apply_async(filterFiles, (jobN,filename, treeModel,debug))
-            # debug model just test 1 file in 1 process
             if debug:
+                # debug model just test 1 file in 1 process
+                # filterFiles(jobN,filename,treeModel,debug)
+                pool.apply_async(filterFiles, (jobN,filename,dataInputPath,resultOutPath,treeModel,debug))
                 break
+            else:
+                pool.apply_async(filterFiles, (jobN,filename,dataInputPath,resultOutPath,treeModel,debug))
             jobN+=1
 
     pool.close()
