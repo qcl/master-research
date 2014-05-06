aa0876c02f515520006c11063855d64d1ea3ecc7
Qing-Cheng Li <qc.linux@gmail.com>
2014-05-07 00:38:02 +0800

    add confusion matrix maker prototype

---
+++ prefix_tree_model/confusionMatrix.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# qcl
+# Build a confusion matrix to improve the performance
+
+import os
+import sys
+import simplejson as json
+
+def confusionMatrixBuilder():
+    pass
+
+if __name__ == "__main__":
+    confusionMatrixBuilder()
+
