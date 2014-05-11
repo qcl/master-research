d76eac9e960c1243aca86d01aae57ce00179a6fd
Qing-Cheng Li <qc.linux@gmail.com>
2014-05-11 17:57:39 +0800

    viewer done version 1

--- prefix_tree_model/evalResultViewer.py
+++ prefix_tree_model/evalResultViewer.py
@@ -6,6 +6,41 @@ import os
 import sys
 import simplejson as json
 
-if len(sys.argv) > 1:
-    result = json.load(open(sys.argv[1]),"r")
+if len(sys.argv) > 2:
+    filepath = sys.argv[1]
+    query = sys.argv[2]
+    if os.path.isdir(filepath): 
+        result = {}
+        for filename in os.listdir(filepath):
+            if ".out" in filename or ".txt" in filename:
+                r = json.load(open(os.path.join(filepath,filename),"r"))
+                for port in r:
+                    if not port.lower() == query.lower():
+                        continue
+                    if not port in result:
+                        result[port] = {"fp":[],"tp":[],"tn":[],"fn":[]}
+                    result[port]["fp"] += r[port]["fp"]
+                    result[port]["tp"] += r[port]["tp"]
+                    result[port]["fn"] += r[port]["fn"]
+                    result[port]["tn"] += r[port]["tn"]
+
+    else:
+        result = json.load(open(filepath),"r")
+
+    for port in result:
+        if port.lower() == query.lower():
+            print port
+            print ""
+            print "True Postive",len(result[port]["tp"])
+            print result[port]["tp"]
+            print ""
+            print "False Negative",len(result[port]["fn"])
+            print result[port]["fn"]
+            print ""
+            print "False Postive",len(result[port]["fp"])
+            print result[port]["fp"]
+            print ""
+            print "True Negative",len(result[port]["tn"])
+            print result[port]["tn"]
+            print ""
     
