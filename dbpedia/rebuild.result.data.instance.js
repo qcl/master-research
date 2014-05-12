133caf2373b846815ccdc043cf9b9669fc210252
Qing-Cheng Li <qc.linux@gmail.com>
2014-05-12 16:06:39 +0800

    rebuild the anwser

---
+++ dbpedia/rebuild.result.data.instance.js
@@ -0,0 +1,32 @@
+/*
+ *  2014.05.12, rebuild the anwser set.
+ * */
+var it = db.old.result.data.instance.find();
+
+while(it.hasNext()){
+    var instance = it.next();
+    var features = [];
+    
+    var mprIt = db.dbpedia.map.property.find({"resource":instance._id})
+    while(mprIt.hasNext()){
+        var property = mprIt.next();
+        var feature = property.property.slice(28);
+        if(features.indexOf(feature)<0){
+            features.push(feature)
+        }
+    }
+
+    var id = instance.id;
+    var revid = instance.revid;
+    var resourceName = instance._id;
+
+    db.result.data.instance.insert({
+        "_id":resourceName,
+        "id":id,
+        "revid":revid,
+        "features":features,
+        "count":features.length
+    });
+
+    print(resourceName+", id="+id+", revid="+revid+", count="+features.length);
+}
