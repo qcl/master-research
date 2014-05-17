68d6da2f827b47f441863b2a06e94b3ff593abd2
Qing-Cheng Li <qc.linux@gmail.com>
2014-05-17 18:02:42 +0800

    add combined viewer

---
+++ wikidump/combinedFilesViewer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# qcl
+# combined files viewer.
+
+import os
+import sys
+import simplejson as json
+
+def combinedFileViewer(filename):
+    
+    content = json.load(open(filename,"r"))
+  
+    for articleName in content:
+        article = content[articleName]
+        print articleName.encode("utf-8")
+        print ""
+        for line in article:
+            print line.encode("utf-8")
+        print "----"
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        combinedFileViewer(filename)
+    else:
+        print "$ python ./combinedFilesViewer.py content.json"
