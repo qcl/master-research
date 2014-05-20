ebae76415ac0c44ffdfbf5e7f49d6a0bc33f0173
Qing-Cheng Li <qc.linux@gmail.com>
2014-05-20 21:49:08 +0800

    debug msg

--- projizz/__init__.py
+++ projizz/__init__.py
@@ -234,12 +234,13 @@ def naiveExtractPatterns(tokens,model,usePos=True):
 
     # deal with the index
     patternFound = sorted(patternFound,key=lambda x:x["start"])
+    print patternFound
     for i in range(0,len(patternFound)):
         prevPtn = patternFound[i]
         if prevPtn["used"] == False:
             continue
 
-        for j in range(i,len(patternFound)):
+        for j in range(i+1,len(patternFound)):
             afterPtn = patternFound[j]
 
             if afterPtn["start"] > prevPtn["to"]:
@@ -256,6 +257,8 @@ def naiveExtractPatterns(tokens,model,usePos=True):
     for ptn in patternFound:
         if ptn["used"]:
             result.append((ptn["ptnId"],ptn["start"],ptn["to"]))
+    
+    print result
 
     return result
 
