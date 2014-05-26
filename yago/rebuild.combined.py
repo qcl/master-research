6934167a0d6add13591f72713da953931dc9c3ac
Qing-Cheng Li <qc.linux@gmail.com>
2014-05-26 15:09:39 +0800

    rebuild.combined.py

--- yago/rebuild.combined.py
+++ yago/rebuild.combined.py
@@ -18,6 +18,51 @@ def combined():
 
     files.sort()
 
+    if not os.path.isdir("/home/ccli/nas-3/yago/yago-all/"):
+        os.mkdir("/home/ccli/nas-3/yago/yago-all")
+    if not os.path.isdir("/home/ccli/nas-3/yago-ptn/yago-ptn-all"):
+        os.mkdir("/home/ccli/nas-3/yago-ptn/yago-ptn-all")
+    count = 0;
+    f = open("/home/ccli/nas-3/yago/yago-all/%05d.json" % (count),"w")
+    fptn = open("/home/ccli/nas-3/yago-ptn/yago-ptn-all/%05d.json" % (count),"w")
+    
+    tmpC = {}
+    tmpPtn = {}
 
+    fc = 0
+    for filename in files:
+        fc += 1
+        print "%s %d / %d" % (filename.split("/")[-1],fc,len(files))
+        g = json.load(open(filename,"r"))
+        if "json-part-a" in filename:
+            gptn = json.load(open("/home/ccli/nas-3/yago-ptn/yago-ptn-a/%s" % (filename.split("/")[-1]),"r"))
+        else:
+            gptn = json.load(open("/home/ccli/nas-3/yago-ptn/yago-ptn-b/%s" % (filename.split("/")[-1]),"r"))
+
+        for key in g: 
+            tmpC[key] = g[key]
+            tmpPtn[key] = gptn[key]
+
+            if len(tmpC) == 1000:
+                print "wirte to %05d.json" % (count)
+                json.dump(tmpC,f)
+                json.dump(tmpPtn,fptn)
+                tmpC = {}
+                tmpPtn = {}
+                f.close()
+                fptn.close()
+                count += 1
+                f = open("/home/ccli/nas-3/yago/yago-all/%05d.json" % (count),"w")
+                fptn = open("/home/ccli/nas-3/yago-ptn/yago-ptn-all/%05d.json" % (count),"w")
+
+    if len(tmpC) > 0: 
+            json.dump(tmpC,f)
+            json.dump(tmpPtn,fptn)
+            tmpC = {}
+            tmpPtn = {}
+            f.close()
+            fptn.close()
+
+    print "write %d files" % (count)
 if __name__ == "__main__":
     combined()
