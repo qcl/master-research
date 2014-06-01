07920efda1849fb42105f76e4e7fdafd505dbf20
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-01 22:29:06 +0800

    fix error

--- yago/run.naive.t5.typed.sh
+++ yago/run.naive.t5.typed.sh
@@ -5,5 +5,5 @@
 
 python ./eval.naive.t5.typed.py /tmp2/ccli/yago-ptn-part-$1/ ./yagoPSv1/ps.$1.json /tmp2/ccli/typed.t5.$1.out
 mkdir -p ~/nas-3/t5-typed
-cp /tmp2/ccli/typed.$1.out ~/nas-3/t5-typed
+cp /tmp2/ccli/typed.t5.$1.out ~/nas-3/t5-typed
 
