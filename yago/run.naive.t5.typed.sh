b4f7f32696e2ad4198fc70a153c773e9bc43c617
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-01 22:01:16 +0800

    top 50 and typed

---
+++ yago/run.naive.t5.typed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# 2014.06.01
+# run ./eval.naive.t5.typed.py 
+
+python ./eval.naive.t5.typed.py /tmp2/ccli/yago-ptn-part-$1/ ./yagoPSv1/ps.$1.json /tmp2/ccli/typed.t5.$1.out
+mkdir -p ~/nas-3/t5-typed
+cp /tmp2/ccli/typed.$1.out ~/nas-3/t5-typed
+
