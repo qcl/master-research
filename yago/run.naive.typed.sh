24f941382e148e796a170e56a94326e519fbaaca
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-01 16:56:50 +0800

    fix typo

--- yago/run.naive.typed.sh
+++ yago/run.naive.typed.sh
@@ -3,7 +3,7 @@
 # 2014.06.01
 # run ./eval.naive.typed.py 
 
-sh ./eval.naive.typed.py /tmp2/ccli/yago-ptn-part-$1/ ./yagoPSv1/ps.$1.json /tmp2/ccli/typed.$1.out
+python ./eval.naive.typed.py /tmp2/ccli/yago-ptn-part-$1/ ./yagoPSv1/ps.$1.json /tmp2/ccli/typed.$1.out
 mkdir -p ~/nas-3/typed
 cp /tmp2/ccli/typed.$1.out ~/nas-3/typed
 
