faa348f1584d0f01d7883596bbbcdcaf46490ae9
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-06 21:53:08 +0800

    再開新局。

---
+++ disambiguate/relaOverlay.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# qcl
+#
+
+def calculateOverlay():
+    
+    # using table information to calculate overlay of each relation
+    
+    pass
+
+if __name__ == "__main__":
+    calculateOverlay()
