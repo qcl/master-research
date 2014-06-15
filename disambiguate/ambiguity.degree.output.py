abff829e890f9d164526819a2bcc562177f96429
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-15 21:03:46 +0800

    add degree

--- disambiguate/ambiguity.degree.output.py
+++ disambiguate/ambiguity.degree.output.py
@@ -10,9 +10,11 @@ def outputStatistics(jsonPath):
     # Read file in.
     properties = projizz.jsonRead(jsonPath)
 
-    for degree in properties:
-        print degree
-
+    for degree in range(1,18):
+        if not degree in properties:
+            print "%d\t%d" % (degree,0)
+        else:
+            print "%d\t%d" % (degree,len(properties[degree]))
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         jsonPath = sys.argv[1]
