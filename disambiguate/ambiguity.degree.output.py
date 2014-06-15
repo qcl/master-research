d83f8314cde0daaed742c89ec8ec1befd319a68d
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-15 21:28:55 +0800

    add occ count

--- disambiguate/ambiguity.degree.output.py
+++ disambiguate/ambiguity.degree.output.py
@@ -10,12 +10,19 @@ def outputStatistics(jsonPath):
     # Read file in.
     properties = projizz.jsonRead(jsonPath)
 
+    occDocs = []
     for degree in range(1,18):
         degree = "%d" % (degree)
         if not degree in properties:
             print "%s\t%d" % (degree,0)
         else:
             print "%s\t%d" % (degree,len(properties[degree]))
+            for ptnId in properties[degree]:
+                for articleId in properties[degree]["occ"]:
+                    if not articleId in occDocs:
+                        occDocs.append(articleId)
+
+            print len(occDocs)
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         jsonPath = sys.argv[1]
