18cec699763b46afac8b342ec38c3cbe9136285f
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-17 00:32:47 +0800

    fix path error

--- selectPattern/run.eval.sh
+++ selectPattern/run.eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-# 2014.06.16
+# 2014.06.17
 # run ./eval.py
 
-python ./eval.py /tmp2/ccli/yago-ptn-part-$1/ /tmp2/ccli/yago-part-$1/ ./ps-c-0/ps.$1.json /home/ccli/nas-3 $1.out 0
+python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-0/ps.$1.json /home/ccli/nas-3 $1.out 0
 
