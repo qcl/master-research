fea26113a0d019ee4c6faa9a6fd463ffd2d0999a
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-17 00:33:29 +0800

    fix path

--- selectPattern/run.eval.sh
+++ selectPattern/run.eval.sh
@@ -3,5 +3,5 @@
 # 2014.06.17
 # run ./eval.py
 
-python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-0/ps.$1.json /home/ccli/nas-3 $1.out 0
+python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-0/ps.$1.json /home/ccli/nas-3/confidence-0 $1.out 0
 
