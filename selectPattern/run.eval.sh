bfd94f10dfa52e9de2ad920ef872f558485a016f
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-20 09:33:44 +0800

    add 0.85

--- selectPattern/run.eval.sh
+++ selectPattern/run.eval.sh
@@ -3,8 +3,9 @@
 # 2014.06.17
 # run ./eval.py
 
-python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-0/ps.$1.json /home/ccli/nas-3/confidence-0 $1.out 0
-python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-7/ps.$1.json /home/ccli/nas-3/confidence-7 $1.out 0.7
-python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-8/ps.$1.json /home/ccli/nas-3/confidence-8 $1.out 0.8
-python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-9/ps.$1.json /home/ccli/nas-3/confidence-9 $1.out 0.9
+python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-0/ps.$1.json /home/ccli/nas-3/confidence-00 $1.out 0
+python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-7/ps.$1.json /home/ccli/nas-3/confidence-70 $1.out 0.7
+python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-8/ps.$1.json /home/ccli/nas-3/confidence-80 $1.out 0.8
+python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-8/ps.$1.json /home/ccli/nas-3/confidence-85 $1.out 0.85
+python ./eval.py /tmp2/ccli/y-ptn-part-$1/ /tmp2/ccli/y-part-$1/ ./ps-c-9/ps.$1.json /home/ccli/nas-3/confidence-90 $1.out 0.9
 
