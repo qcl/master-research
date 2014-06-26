8331226557e703c86b3f45236cbe625f93cc8383
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-26 20:58:39 +0800

    add run scripts

---
+++ vectorSpaceModel/build.tfidf.sh
@@ -0,0 +1,4 @@
+python ./genTokens.py /home/ccli/tmp2/ma-00-$1 /home/ccli/tmp2/tfidf-00-$1
+python ./genTokens.py /home/ccli/tmp2/ma-70-$1 /home/ccli/tmp2/tfidf-70-$1
+python ./genTokens.py /home/ccli/tmp2/ma-80-$1 /home/ccli/tmp2/tfidf-80-$1
+python ./genTokens.py /home/ccli/tmp2/ma-90-$1 /home/ccli/tmp2/tfidf-90-$1
