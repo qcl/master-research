82b39909c4905145c46f879a0a4d2c12e1b1cc46
Qing-Cheng Li <qc.linux@gmail.com>
2014-06-30 17:21:34 +0800

    top 200

---
+++ vectorSpaceModel/build.top.tfidf.sh
@@ -0,0 +1,4 @@
+python ./genTopNTokens.py /home/ccli/tmp2/ma-00-$1 200 /home/ccli/tmp2/tfidf-200-00-$1
+python ./genTopNTokens.py /home/ccli/tmp2/ma-70-$1 200 /home/ccli/tmp2/tfidf-200-70-$1
+python ./genTopNTokens.py /home/ccli/tmp2/ma-80-$1 200 /home/ccli/tmp2/tfidf-200-80-$1
+python ./genTopNTokens.py /home/ccli/tmp2/ma-90-$1 200 /home/ccli/tmp2/tfidf-200-90-$1
