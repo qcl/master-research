47bbe0c1cb1ac69552cbaf48cebbf23379bdda4d
Qing-Cheng Li <qc.linux@gmail.com>
2014-07-03 21:11:18 +0800

    prepare to build nbc

---
+++ nbc/build.nbc.model.sh
@@ -0,0 +1,4 @@
+python ./buildNBCs.py /home/ccli/tmp2/ma-00-$1 /home/ccli/tmp2/nbc-00-$1
+python ./buildNBCs.py /home/ccli/tmp2/ma-70-$1 /home/ccli/tmp2/nbc-70-$1
+python ./buildNBCs.py /home/ccli/tmp2/ma-80-$1 /home/ccli/tmp2/nbc-80-$1
+python ./buildNBCs.py /home/ccli/tmp2/ma-90-$1 /home/ccli/tmp2/nbc-90-$1
---
+++ nbc/buildNBCs.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# qcl
+# build naive bayes models from dir
+
+import os
+import sys
+import projizz
+
+def buildModels(inputpath,outputPath):
+
+    projizz.checkPath(outputPath)
+
+    relations = projizz.getYagoRelation()
+    for relation in relations:
+        if relation == "produced":
+            continue
+
+        posInstances = projizz.jsonRead( os.path.join(inputpath,"%s.pos" % (relation)) )
+        negInstances = projizz.jsonRead( os.path.join(inputpath,"%s.neg" % (relation)) )
+
+        instances = []
+        for data in posInstances:
+            instances.append( (data["text", data["label"]]) )
+        for data in negInstances:
+            instances.append( (data["text", data["label"]]) )
+
+        classifier = projizz.NaiveBayesClassifier(instances)
+        classifier.save( os.path.join(outputPath,"%s.nbc" % (relation)) )
+        print "Write to %s %s.nbc" % (outputPath,relation)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        modelsDir = sys.argv[1]
+        outputPath = sys.argv[2]
+        buildModels(modelsDir,outputPath)
+    else:
+        print "$ python ./buildNBCs.py [inpu model dir] [output model dir]"
