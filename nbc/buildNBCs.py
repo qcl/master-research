26a83ef05105a7a0c68f5319aaccf797d1beb6b3
Qing-Cheng Li <qc.linux@gmail.com>
2014-07-04 22:07:56 +0800

    平行化分類器訓練過程。

--- nbc/buildNBCs.py
+++ nbc/buildNBCs.py
@@ -5,35 +5,50 @@
 import os
 import sys
 import projizz
+import multiprocessing
+
+def trainModel(jobid,relation,inputpath,outputPath):
+    print "%d -> train %s" % (jobid,relation)
+    instances = []
+    filepath = os.path.join(inputpath,"%s.pos" % (relation))
+    if os.path.exists(filepath):
+        posInstances = projizz.jsonRead( filepath )
+        for data in posInstances:
+            instances.append( (data["text"], data["label"]) )
+    filepath = os.path.join(inputpath,"%s.neg" % (relation))
+    if os.path.exists(filepath):
+        negInstances = projizz.jsonRead( filepath )
+        for data in negInstances:
+            instances.append( (data["text"], data["label"]) )
+
+    if len(instances) == 0:
+        print "Cannot build %s.nbc because there are no training data." % (relation)
+
+    classifier = projizz.NaiveBayesClassifier(instances)
+    classifier.save( os.path.join(outputPath,"%s.nbc" % (relation)) )
+    print "%d -> Write to %s %s.nbc" % (jobid,outputPath,relation)
 
 def buildModels(inputpath,outputPath):
 
     projizz.checkPath(outputPath)
 
+    cpuCount = multiprocessing.cpu_count()
+    if cpuCount > 8:
+        cpuCount = 8
+
+    pool = multiprocessing.Pool(processes=cpuCount) 
+    t = 0
+
     relations = projizz.getYagoRelation()
     for relation in relations:
         if relation == "produced":
             continue
+        pool.apply_async(trainModel, (t,relation,inputpath,outputPath))
+        t += 1
+    pool.close()
+    pool.join()
 
-        instances = []
-        filepath = os.path.join(inputpath,"%s.pos" % (relation))
-        if os.path.exists(filepath):
-            posInstances = projizz.jsonRead( filepath )
-            for data in posInstances:
-                instances.append( (data["text"], data["label"]) )
-        filepath = os.path.join(inputpath,"%s.neg" % (relation))
-        if os.path.exists(filepath):
-            negInstances = projizz.jsonRead( filepath )
-            for data in negInstances:
-                instances.append( (data["text"], data["label"]) )
-
-        if len(instances) == 0:
-            print "Cannot build %s.nbc because there are no training data." % (relation)
-            continue
-
-        classifier = projizz.NaiveBayesClassifier(instances)
-        classifier.save( os.path.join(outputPath,"%s.nbc" % (relation)) )
-        print "Write to %s %s.nbc" % (outputPath,relation)
+    print "Done training all classifiers"
 
 if __name__ == "__main__":
     if len(sys.argv) > 2:
